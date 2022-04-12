12a197de07e77c968fa7c006c5ac04afdc878403
sanam.udash <udashsanam@gmail.com>
2022-04-12 07:03:45 +0545

    add: scroll view and checkbox

--- app/src/main/java/com/example/merofirstapp/LoginActivity.java
+++ app/src/main/java/com/example/merofirstapp/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.example.merofirstapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class LoginActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 //    activity life cycle
 //    1. onCreate  when activity crate
 //    2. onStart   when activity launched
@@ -17,5 +19,21 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        link between layout and activity file
         setContentView(R.layout.layout_login);
+        initToolBar();
+
+    }
+
+
+    // code for the tool bar
+    private void initToolBar(){
+        // R specify the resource file
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("New Mero First App");
+        // display back bottom in the tool bar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+
     }
 }
---
+++ app/src/main/java/com/example/merofirstapp/SignUpActivity.java
@@ -0,0 +1,12 @@
+package com.example.merofirstapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
